267d7ab01c95e473311d7b98526652ed592eb38d
David Ntwari <d.ntwari@alustudent.com>
2023-08-09 17:40:20 +0200

    wow

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace ArchitectArithmetic
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+
+    double mexico = Teotihuacan.areaOfTeotihuacan();
+    double saudiArabia = MosqueOfMecca.areaOfMosqueOfMecca();
+    double india = TajMahal.areaOfTajMahal();
+
+
+    Console.WriteLine($"The Teotihucan Located in Mexico has an Area of {mexico}");
+    Console.WriteLine($"The Great Mosque of Mecca Located in Saudi Arabia has an Area of {saudiArabia}");
+    Console.WriteLine($"The Taj Mahal Located in India has an Area of {india}");
+  }
+  
+}
+
+
+public class Teotihuacan {
+    static double triangle1(double bottom, double height) {
+    return (.5 * bottom * height);
+  }
+
+  static double circle1 (double radius){
+    return (Math.PI * Math.Pow(radius, 2));
+  }
+
+  static double rectangle1 (double length, double width){
+    return(length * width);
+  }
+
+  public static double areaOfTeotihuacan(){
+    
+    double areaTriangle1 = triangle1(500,750);
+    double areaCircle1 = circle1(375);
+    double areaRectangle1 = rectangle1(1500,2500);
+
+    return areaCircle1 + areaRectangle1 + areaTriangle1;
+
+  }
+
+}
+
+public class MosqueOfMecca {
+    static double triangle2(double bottom, double height) {
+    return (.5 * bottom * height);
+  }
+
+    static double smallRectangle (double length, double width){
+    return (length * width);
+  }
+
+    static double largeRectangle (double length, double width){
+    return(length * width);
+  }
+
+    public static double areaOfMosqueOfMecca(){
+    
+    double areatriangle2 = triangle2(84,264);
+    double areaSmallRectangle = smallRectangle(180,106);
+    double areaLargeRectangle = largeRectangle(284,264);
+
+    return (areaLargeRectangle - areatriangle2) + areaSmallRectangle;
+
+  }
+
+}
+
+
+
+public class TajMahal {
+    static double triangles(double bottom, double height) {
+    return (.5 * bottom * height) * 4;
+  }
+
+    static double square (double side){
+    return (side * side);
+  }
+
+    public static double areaOfTajMahal(){
+    
+    double areatriangles = triangles(24,24);
+    double areasquare = square(90.5);
+
+    return (areasquare - areatriangles);
+
+  }
+
+}
+}
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# calculate_area
+# Architectural Structure Area Calculator
+This C# program calculates and displays the areas of three iconic architectural structures: Teotihuacan, the Mosque of Mecca, and the Taj Mahal. Each structure's area is computed by combining the areas of various geometric shapes, such as triangles, circles, and rectangles.
+
+## How to Use
+Clone or download the repository containing the code.
+
+Open the solution or project in your preferred C# development environment.
+
+Run the program to see the calculated areas of the architectural structures.
+
+## Code Structure
+The program is organized into classes, each representing a specific architectural structure and containing methods for calculating area components.
+
+Teotihuacan Class: Calculates the area of Teotihuacan using methods for triangles, circles, and rectangles.
+![Teotihuacan](teotihuacan.png)
+
+
+
+MosqueOfMecca Class: Computes the area of the Mosque of Mecca, considering triangles and rectangles.
+![Mosque of Mecca](great_mosque_of_mecca.png)
+
+
+
+TajMahal Class: Calculates the area of the Taj Mahal by combining triangles and squares.
+![Taj Mahal](taj_mahal.png)
+
+
+Program Class: Contains the main method to run the program. It calculates the areas of the three structures and prints the results.
+
+
+## Contributing
+Feel free to contribute enhancements, bug fixes, or other improvements to this code. Submit a pull request to share your changes.
+
+#### Thanks for stoping by, Happy Coding.
+
+
